f7bfdfee261b11defaccaddf3b1b24f916c45a02
Marton Lederer <marton@lederer.hu>
2024-02-03 17:41:29 +0100

    feat: add global variables

--- library/environment.lua
+++ library/environment.lua
@@ -7,3 +7,27 @@ ao = require "ao"
 
 -- "Handlers" global library
 Handlers = require ".handlers"
+
+-- All the messages that are received and not handled by any handlers
+---@type Message[]
+Inbox = {}
+
+-- Send a message to another process
+Send = ao.send
+
+-- Spawn a process
+Spawn = ao.spawn
+
+-- A string that is set on init that describes the name of your process
+---@type string
+Name = ""
+
+-- The address of the owner of this process
+---@type string
+Owner = ""
+
+-- A function that takes any lua Table and generates a print friendly output of the data
+Dump = require ".dump"
+
+-- A functional utility library with functions like map, reduce, filter
+Utils = require ".utils"
