27bec671c4ef2229c0b0e8ba78afab742165c29f
Marton Lederer <marton@lederer.hu>
2024-02-03 16:54:16 +0100

    feat: dump

---
+++ library/dump.lua
@@ -0,0 +1,11 @@
+---@meta .dump
+-- Dump a table as a string
+---@param val any
+---@param indent number
+---@param padding number
+---@param filter number
+---@param udata any
+---@return string
+local dump = function (val, indent, padding, filter, udata) end
+
+return dump
